1a934392f0bde100df1d5194a20ffd740fc0148a
renskebingen <renske.bingen@gmail.com>
2026-06-10 12:57:13 +0200

    styling

--- static/js/tijdlijn.js
+++ static/js/tijdlijn.js
@@ -28,3 +28,93 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+// Navigation indicator
+
+const links = document.querySelectorAll(".nav-links a");
+const indicator = document.querySelector(".nav-indicator");
+
+if (links.length && indicator) {
+    let activeLink = document.querySelector(".nav-links a.active");
+
+    function moveIndicator(link) {
+        indicator.style.left = `${link.offsetLeft}px`;
+        indicator.style.width = `${link.offsetWidth}px`;
+    }
+
+    moveIndicator(activeLink);
+
+  links.forEach(link => {
+
+      link.addEventListener("mouseenter", () => {
+
+          if (activeLink && link !== activeLink) {
+              activeLink.classList.add("temp-inactive");
+          }
+
+          moveIndicator(link);
+      });
+
+      link.addEventListener("mouseleave", () => {
+
+          if (activeLink) {
+              activeLink.classList.remove("temp-inactive");
+          }
+
+          moveIndicator(activeLink);
+      });
+
+  });
+}
+
+// =============================
+// DONATIE TABS
+// =============================
+
+const tabs = document.querySelectorAll(".donatie-tabs p");
+const tabIndicator = document.querySelector(".tab-indicator");
+
+if (tabs.length && tabIndicator) {
+
+    let activeTab = document.querySelector(".donatie-tabs p.active");
+
+    function moveTabIndicator(tab) {
+        tabIndicator.style.left = `${tab.offsetLeft}px`;
+        tabIndicator.style.width = `${tab.offsetWidth}px`;
+    }
+
+    moveTabIndicator(activeTab);
+
+    tabs.forEach(tab => {
+
+        tab.addEventListener("mouseenter", () => {
+
+            if (activeTab && tab !== activeTab) {
+                activeTab.classList.add("temp-inactive");
+            }
+
+            moveTabIndicator(tab);
+        });
+
+        tab.addEventListener("mouseleave", () => {
+
+            if (activeTab) {
+                activeTab.classList.remove("temp-inactive");
+            }
+
+            moveTabIndicator(activeTab);
+        });
+
+        tab.addEventListener("click", () => {
+
+            activeTab.classList.remove("active");
+
+            tab.classList.add("active");
+
+            activeTab = tab;
+
+            moveTabIndicator(activeTab);
+        });
+
+    });
+}
