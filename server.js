4c547c296aee343a21cbb23f1341afc3c22a857c
renskebingen <renske.bingen@gmail.com>
2026-06-18 11:20:48 +0200

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 
-const Farm = require("./models/Farm")
+const Farm = require("./models/farm")
 const Verkooppunt = require("./models/Verkooppunt")
 const boerenData = require("./static/data/boeren.json")
 
